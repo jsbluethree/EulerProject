07c4bc21d6a0cd59a630141d56c5ef18ea69ca3f
Chris Bowers <jsbluethree@gmail.com>
2016-08-17 23:01:00 -0400

    slight syntactic change cuz it bothered me

--- euler010.scm
+++ euler010.scm
@@ -3,17 +3,14 @@
 
 #lang scheme
 
-(require srfi/1)
+(define (sieve divisor in out final)
+  (if (null? in)
+      (let ((next (reverse out)))
+        (if (null? next)
+            (cons divisor final)
+            (sieve (car next) next '() (cons divisor final))))
+      (if (zero? (modulo (car in) divisor))
+          (sieve divisor (cdr in) out final)
+          (sieve divisor (cdr in) (cons (car in) out) final))))
 
-(define sieve
-  (lambda (divisor in out final)
-    (if (null? in)
-        (let ((next (reverse out)))
-          (if (null? next)
-              (cons divisor final)
-              (sieve (car next) next '() (cons divisor final))))
-        (if (zero? (modulo (car in) divisor))
-            (sieve divisor (cdr in) out final)
-            (sieve divisor (cdr in) (cons (car in) out) final)))))
-
-(foldl + 0 (sieve 2 (iota 1999999 2) '() '()))
+(foldl + 0 (sieve 2 (range 2 2000000) '() '()))
